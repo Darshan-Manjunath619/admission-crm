dc3933b192ace31026c5d49eaa7796baa9b435b4
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2026-02-25 08:55:29 +0530

    Added all the transactions and JPA repos

--- crm/src/main/java/com/admission/crm/AdmissionCrmApplication.java
+++ crm/src/main/java/com/admission/crm/AdmissionCrmApplication.java
@@ -2,12 +2,17 @@ package com.admission.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.admission.crm")
+@EnableJpaRepositories(basePackages = "com.admission.crm.repository")
+@EnableTransactionManagement
 public class AdmissionCrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdmissionCrmApplication.class, args);
+		System.out.println("✅ Admission CRM Application Started Successfully");
 	}
 
 }
