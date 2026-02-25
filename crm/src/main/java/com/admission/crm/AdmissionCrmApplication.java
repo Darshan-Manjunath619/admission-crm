ef8fd4cdcc922477cd2a1ae71471d18e67862df0
Darshan-Manjunath619 <darshan.manjunath619@gmail.com>
2026-02-25 08:34:20 +0530

    Initial project setup: Spring Boot base, application.yml, pom.xml, folder structure

---
+++ crm/src/main/java/com/admission/crm/AdmissionCrmApplication.java
@@ -0,0 +1,13 @@
+package com.admission.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionCrmApplication.class, args);
+	}
+
+}
---
+++ crm/src/test/java/com/admission/crm/AdmissionCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admission.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
